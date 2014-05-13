329e85edab6057ab023fed2a639874414fe4d7c9
Miles Wang <mw10104587@gmail.com>
2014-05-13 09:32:44 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheckinMap::Application.config.secret_key_base = '9d734760463950f351dfa5b9e7adf34cb53060755a067b0f57cc1f2486d79bfa3fc16039d1fdf873240533de1f1296218fecdb6fef5e0c24c0e1e2fde285d736'
